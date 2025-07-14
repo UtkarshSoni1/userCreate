3d01645c8a4a6e86b8c7718530e7929fdad9ce89
UtkarshSoni1 <utkarshsoni2222@gmail.com>
2025-07-14 20:08:06 +0530

    Fix image upload: Use Cloudinary storage instead of local temp storage

--- config/multer-config.js
+++ config/multer-config.js
@@ -1,29 +1,12 @@
-// const cloudinary = require('cloudinary').v2;
-// const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('./cloudinary');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
-
-// const storage = new CloudinaryStorage({
-//   cloudinary: cloudinary,
-//   params: {
-//     folder: 'userCreateUploads', // folder name in Cloudinary
-//     allowed_formats: ['jpg', 'jpeg', 'png', 'gif'],
-//   },
-// });
-
-// module.exports = { cloudinary, storage };
-const multer = require('multer');
-
-const storage = multer.diskStorage({
-  filename: function (req,file,cb) {
-    cb(null, file.originalname)
-  }
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'userCreateUploads',
+    allowed_formats: ['jpg', 'jpeg', 'png', 'gif'],
+  },
 });
 
-const upload = multer({storage: storage});
-
-module.exports = upload;
+module.exports = { storage };
