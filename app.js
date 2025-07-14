be416e0d35d6c8003920562fb492ffe70ce2b314
UtkarshSoni1 <utkarshsoni2222@gmail.com>
2025-07-14 17:24:48 +0530

    fixes

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 
 app.set('view engine','ejs');
