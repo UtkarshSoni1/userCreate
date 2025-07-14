995afc52089e17ad293975b2b4736d99e51afc32
UtkarshSoni1 <utkarshsoni2222@gmail.com>
2025-07-14 16:30:03 +0530

    fixed the image problem

--- app.js
+++ app.js
@@ -12,22 +12,12 @@ const { hash } = require('node:crypto');
 const { type } = require('node:os');
 const multer = require('multer');
 const crypto = require('node:crypto')
+const multer = require('multer');
+const { storage } = require('./config/multer-config'); 
+const upload = multer({ storage: storage });
 
 app.use(cookieParser());
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './public/images/uploads')
-  },
-  filename: function (req, file, cb) {
-    crypto.randomBytes(12, function(err,bytes){
-        const fn = bytes.toString("hex") + path.extname(file.originalname);
-        cb(null, fn);
-    })
-  }
-})
-
-const upload = multer({storage : storage})
 
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
